909003df36d9aa1f031bd6a296b04d3e58c808d8
r-stemmle <ryan.stemmle@gmail.com>
2021-04-03 17:50:53 -0400

    Refactor spec examples

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'application show page' do
   before :each do
+    @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @carol = Application.create!(
       name: "Carol Smith",
       street: "153 High Crest rd",
       city: "Pelham",
       state: "AL",
       zip_code: "35124",
-      justification: "Big yard and I work from home")
+     )
   end
   describe 'when I visit the applications show page' do
-    it "it displays name, address, description, pets, status" do
+    it "it displays name, address, justification, pets, status" do
       #dependencies: pets
       visit "/applications/#{@carol.id}"
 
@@ -19,7 +20,6 @@ RSpec.describe 'application show page' do
       expect(page).to have_content(@carol.city)
       expect(page).to have_content(@carol.state)
       expect(page).to have_content(@carol.zip_code)
-      expect(page).to have_content(@carol.justification)
       expect(page).to have_content("In Progress")
     end
   end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:applications_pets) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+  end
 end
